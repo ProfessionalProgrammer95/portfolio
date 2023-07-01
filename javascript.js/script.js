7e4381825791a63e7d4c9087d8d6c3340ea1db68
ProfessionalProgrammer95 <rakshithav1358@gmail.com>
2023-07-01 18:26:04 +0530

    portfolio

---
+++ javascript.js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('.button').click(function(){
+        $($(this).data("target")).addClass("page--active");
+    });
+    $('.page-close').click(function(){
+        $('.page').removeClass('page--active');
+    });
+})
